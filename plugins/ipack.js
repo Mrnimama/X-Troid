14dbd4f5ac2fbf0001be1751eb50bac3f5e9b64e
ᴍʀ ɴɪᴍᴀ 2ɴᴅ ɢɪᴛ <103928995+MRnimaofc2@users.noreply.github.com>
2022-04-20 15:36:33 +0530

    🐺 x-ᴛʀᴏɪᴅ ɴɪᴍᴀ 🐺

--- plugins/ipack.js
+++ plugins/ipack.js
@@ -318,7 +318,7 @@ else if (Config.WORKTYPE == 'public') {
     }));
 
     
-    XTroid.addCMD({pattern: 'bot', fromMe: false, deleteCommand: false,desc:'Get info aboout bot'}, (async (message, match) => {
+    XTroid.addCMD({pattern: 'xtroid', fromMe: false, deleteCommand: false,desc:'Get info aboout bot'}, (async (message, match) => {
 var lasiyasimg = await axios.get(`https://telegra.ph/file/145ee2d4fb11363f72582.jpg`, { responseType: 'arraybuffer' })
       await message.client.sendMessage(message.jid, Buffer.from(lasiyasimg.data), MessageType.image, {mimetype: Mimetype.png, caption: '                  *♔ 𝓧-𝓣𝓻𝓸𝓲𝓭 ♔* \n\n\n❍ 𝗛𝗼𝘀𝘁𝗲𝗱 ➤ 𝘼𝙒𝙎\n❍ 𝗩𝗲𝗿𝘀𝗶𝗼𝗻 ➤ 𝐕'+Config.VERSION+'\n❍ 𝗕𝗿𝗮𝗻𝗰𝗵  ➤ *'+Config.BRANCH+'*\n❍ 𝗔𝗯𝗼𝘂𝘁    ➤ 𝙡𝙖𝙨𝙞𝙮𝙖.𝙢𝙡\n❍ 𝗢𝘄𝗻𝗲𝗿   ➤𝙩.𝙢𝙚/𝙡𝙖𝙨𝙞99𝙓\n❍ 𝗠𝗼𝗱𝗲𝗿𝘀 ➤┓\n                        ┣ @W4RR10R\n                        ┣ @SL_MGx03\n                        ┣ @lucy_99X\n❍ 𝗠𝗮𝗶𝗻 𝗚𝗿𝗼𝘂𝗽 ➤ t.me/danuma01\n\n❍ 𝐓𝐮𝐭𝐞 ➤ https://cutt.ly/hmtJU0u\n⊱ ──────    ♔    ────── ⊰\n'})
     }));
