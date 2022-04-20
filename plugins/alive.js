13d407723016b76b9e412a236c7740657ae54b7f
ᴍʀ ɴɪᴍᴀ 2ɴᴅ ɢɪᴛ <103928995+MRnimaofc2@users.noreply.github.com>
2022-04-20 07:40:44 +0530

    🐺 x-ᴛʀᴏɪᴅ ɴɪᴍᴀ 🐺

--- plugins/alive.js
+++ plugins/alive.js
@@ -7,5 +7,5 @@ let FM = Config.WORKTYPE == 'public' ? false : true
 XTroid.addCMD({pattern: 'alive', fromMe: FM, deleteCommand: true,}, (async (message, match) => {
 
 var lasi = await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' })
-await message.client.sendMessage(message.jid, Buffer.from(lasi.data), MessageType.image, {mimetype: Mimetype.png, caption: '            '+Config.ALIVETEXT+'\n *x-ᴛʀᴏɪᴅ ᴛᴇsᴛ ᴇᴅɪᴛɪᴏɴ | ᴍʀ ɴɪᴍᴀ*'})
+await message.client.sendMessage(message.jid, Buffer.from(lasi.data), MessageType.image, {mimetype: Mimetype.png, caption: '            '+Config.ALIVETEXT+'\n\n*x-ᴛʀᴏɪᴅ ᴛᴇsᴛ ᴇᴅɪᴛɪᴏɴ | ᴍʀ ɴɪᴍᴀ*'})
     }));
